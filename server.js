312221f203dd435875b9e51ad7d06a39786dd77c
Sam Brandt <sbrandt1240@gmail.com>
2019-02-04 15:36:05 +0200

    changed textare back to input, added trash icon, fixed time,

--- server.js
+++ server.js
@@ -13,6 +13,29 @@ app.use(bodyParser.json())
 app.use(express.static('public'))
 
 var db;
+
+function time() {
+  var d = new Date();
+  var s = d.getSeconds();
+  var m = d.getMinutes();
+  var h = d.getHours();
+  var h = ("0" + h).slice(-2);
+  var month = d.getMonth() + 1;
+  var day = d.getDate();
+  m = checkTime(m);
+  s = checkTime(s);
+  return h + ":" + m + " | " + day + "/" + month;
+}
+setInterval(time, 1000);
+
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i
+  }; // add zero in front of numbers < 10
+  return i;
+}
+
+
 MongoClient.connect('mongodb://public:public50@ds121295.mlab.com:21295/chatapp', {
     useNewUrlParser: true
   },
@@ -27,26 +50,32 @@ MongoClient.connect('mongodb://public:public50@ds121295.mlab.com:21295/chatapp',
 app.get('/', function(req, res) {
   db.collection('quotes').find().toArray((err, result) => {
     if (err) return console.log(err)
-    var time = new Date();
-    var now = ( /*time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds() + '   ' + */ time.getDate() + "/" + time.getMonth() + "/" + time.getFullYear());
     res.render('index.ejs', {
-      quotes: result,
-      now
+      quotes: result
     })
     console.log(result)
   })
 })
 
 app.post('/quotes', (req, res) => {
-  db.collection('quotes').insertOne(req.body, (err, result) => {
+  var reqreq = req.body
+  reqreq.time = time()
+  db.collection('quotes').insertOne(reqreq, (err, result) => {
     if (err) return console.log(err)
 
     console.log('saved to database')
+
     res.redirect('/')
   })
   //console.log(req.body)
 })
 
+app.post('delete', (req, res) => {
+      db.collection("quotes").deleteMany({}, function(err, obj) {
+        if (err) throw err;
+        console.log(obj.result.n + " document(s) deleted");
+        db.close();
+      });
+    });
 
-
-console.log("Yalla, you're up and running");
+    console.log("Yalla, you're up and running");
